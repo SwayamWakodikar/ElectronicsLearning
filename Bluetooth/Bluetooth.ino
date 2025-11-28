2099005ae4603c89f68b8f70bec1732a7799f840
SwayamWakodikar <swayam.w06@gmail.com>
2025-11-28 21:59:39 +0530

    Tried for working with display and LED lights

--- Bluetooth/Bluetooth.ino
+++ Bluetooth/Bluetooth.ino
@@ -1,9 +1,35 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup() {
-  // put your setup code here, to run once:
-`
+  Serial.begin(115200);
+
+  // Start I2C
+  Wire.begin(21, 22);   // SDA = 21, SCL = 22
+
+  // Initialize display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("Display not found!");
+    for(;;);
+  }
+
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+
+  display.println("Hello");
+  display.println("World!");
+
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
